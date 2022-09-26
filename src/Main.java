ce8bb5b1cb3784b87b5374262208a6c01b30ca7a
sruiNascimento <sruy19@gmail.com>
2022-09-26 18:08:37 -0300

    primeiro dia finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        //Lendo
+        Properties props =  new Properties();
+        props.load(new FileReader("confs.properties"));
+        final String API_KEY = props.getProperty("API_KEY");
+
+        try {
+            var uri = URI.create("https://imdb-api.com/en/API/Top250Movies/"+API_KEY);
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(uri).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.body());
+            System.out.println(response.body().length());
+
+            System.out.println(response.body().getClass().getName());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
